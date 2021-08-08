2886c9f07b92311220cf5ad3beace8d04d298dab
Emi <bati_caramusa@hotmail.com>
2021-08-08 15:41:31 -0300

    Modificaciones0808

--- LanGuardianCont/views.py
+++ LanGuardianCont/views.py
@@ -35,25 +35,32 @@ def configuracion(request):
     contenido = f.read()
     f.close()    
     parse = CiscoConfParse("config.txt")
-    lista_reco=[]
-    aaauth = parse.find_objects(r"^aaa")
+    p1 = parse.find_objects(r"^aaa new-model")
+
+    if (len(p1) == 0):
+        desc = "Habilite el sistema de control de acceso AAA"
+        razon = "Los servicios de autenticación, autorización y contabilidad (AAA) proporcionan una fuente autorizada para administrar y monitorear el acceso de los dispositivos. La centralización del control mejora la coherencia del control de acceso, los servicios a los que se puede acceder una vez autenticados y la responsabilidad mediante el seguimiento de los servicios a los que se accede. Además, la centralización del control de acceso simplifica y reduce los costos administrativos de aprovisionamiento y desaprovisionamiento de cuentas, especialmente cuando se administra una gran cantidad de dispositivos"
+        impacto = "La implementación de Cisco AAA es significativamente disruptiva, ya que los métodos de acceso anteriores se desactivan de inmediato. Por lo tanto, antes de implementar Cisco AAA, la organización debe revisar y planificar cuidadosamente sus criterios de autenticación (inicios de sesión y contraseñas, desafíos y respuestas, y tecnologías de token), métodos de autorización y requisitos de contabilidad"
+        remediacion = "Ejecute el comando 'aaa new-model'"
+        lista_reco = {'descripcion':desc, 'razon':razon,'impacto':impacto,'remediacion':remediacion}        
+    print(lista_reco)
     
-    if (len(aaauth) == 0):
+    #if (len(aaauth) == 0):
         
-        print("No esta configurado")
-        print("Se recomienda configurar el servicio AAA porque XXXXXXXXXX")
-        print("Para realizarlo debe ejecutar la sentencia: XXXXXXXXXXX")
-    else:
-        #print(aaauth)
-        for obj in aaauth:
-            if "authentication" in obj.text:
-                print("Tiene configurada authentication")
-            if "authorization" in obj.text:
-                print("Tiene configurada authorization")
-            if "accounting" in obj.text:
-                print("Tiene configurada accounting")
-            if "session-id" in obj.text:
-                print("Tiene configurada session-id")
-            if "new-model" in obj.text:
-                print("Tiene configurada new-model")
-    return render(request,"LanGuardianCont/configuracion.html", {'cont':contenido})
+    #    print("No esta configurado")
+    #    print("Se recomienda configurar el servicio AAA porque XXXXXXXXXX")
+    #    print("Para realizarlo debe ejecutar la sentencia: XXXXXXXXXXX")
+    #else:
+    #    #print(aaauth)
+    #    for obj in aaauth:
+    #        if "authentication" in obj.text:
+    #            print("Tiene configurada authentication")
+    #        if "authorization" in obj.text:
+    #            print("Tiene configurada authorization")
+    #        if "accounting" in obj.text:
+    #            print("Tiene configurada accounting")
+    #        if "session-id" in obj.text:
+    #            print("Tiene configurada session-id")
+    #        if "new-model" in obj.text:
+    #            print("Tiene configurada new-model")
+    return render(request,"LanGuardianCont/configuracion.html", {'cont':contenido, 'lis':lista_reco})
